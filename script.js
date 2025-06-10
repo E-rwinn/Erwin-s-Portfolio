05f05a4708eee0c169d7fa5e4c3ad7038a8bbc61
Erwin <erwin.ucelo@gmail.com>
2025-06-10 09:39:54 -0400

    New Project Section UI

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const navLinks = document.getElementById('nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+//Project Section//
+ const loadMoreBtn = document.getElementById('loadMoreBtn');
+    const boxes = document.querySelectorAll('.project-box');
+    let current = 6;
+
+    loadMoreBtn.addEventListener('click', () => {
+      const next = current + 3;
+      for (let i = current; i < next && i < boxes.length; i++) {
+        boxes[i].classList.add('show');
+      }
+      current = next;
+      if (current >= boxes.length) {
+        loadMoreBtn.style.display = 'none';
+      }
+    });
